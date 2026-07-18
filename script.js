edbfe45a2ee2eff5b2e4798bede0d6d47eda1274
apolinarcurtido09-ctrl <apolinarcurtido09@gmail.com>
2026-07-17 22:41:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,36 +2,49 @@ const input = document.getElementById("meshInput");
 const button = document.getElementById("readBtn");
 const info = document.getElementById("info");
 
-function leerFloat(bytes, offset) {
+function leerFloat(bytes, offset){
+
     const view = new DataView(bytes.buffer);
-    return view.getFloat32(offset, true);
+
+    return {
+        little: view.getFloat32(offset, true),
+        big: view.getFloat32(offset, false)
+    };
+
 }
 
-button.onclick = async () => {
+button.onclick = async ()=>{
 
     const file = input.files[0];
 
-    if (!file) {
-        info.textContent = "No seleccionaste ningún archivo.";
+    if(!file){
+        info.textContent="No seleccionaste ningún archivo.";
         return;
     }
 
     const buffer = await file.arrayBuffer();
+
     const bytes = new Uint8Array(buffer);
 
-    let texto = "";
+    let texto="";
 
-for(let i = 0; i < 256; i += 4){
+    for(let i=0;i<128;i+=4){
 
-    let valor = leerFloat(bytes, i);
+        const f=leerFloat(bytes,i);
 
-    texto +=
-        "Offset "
-        + i.toString().padStart(3," ")
-        + " = "
-        + valor
-        + "\n";
+        texto +=
+`Offset ${i}
 
-}
+Little: ${f.little}
 
-info.textContent = texto;
+Big: ${f.big}
+
+----------------
+
+`;
+
+    }
+
+    info.textContent=texto;
+
+};
