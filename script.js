550fb1a1540b5f47fd2eb7986495094866e4b6e5
apolinarcurtido09-ctrl <apolinarcurtido09@gmail.com>
2026-07-17 21:36:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-document.querySelector("button").onclick=()=>{
-    alert("Todavía no está implementado.");
+const input = document.getElementById("meshInput");
+const button = document.getElementById("readBtn");
+const info = document.getElementById("info");
+
+button.onclick = async ()=>{
+
+const file = input.files[0];
+
+if(!file){
+
+alert("Selecciona un archivo.");
+
+return;
+
 }
+
+const buffer = await file.arrayBuffer();
+
+const bytes = new Uint8Array(buffer);
+
+// Intentamos leer los primeros 32 bytes como texto
+
+const header = new TextDecoder().decode(
+bytes.slice(0,32)
+);
+
+info.innerHTML=
+`
+Archivo:
+
+${file.name}
+
+Tamaño:
+
+${file.size} bytes
+
+Cabecera:
+
+${header}
+`;
+
+};
