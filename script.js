c1a5db6a7215f385e03b6ca3744402af159380af
apolinarcurtido09-ctrl <apolinarcurtido09@gmail.com>
2026-07-17 22:10:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const input = document.getElementById("meshInput");
 const button = document.getElementById("readBtn");
 const info = document.getElementById("info");
 
-button.onclick = () => {
-    info.textContent = "¡El botón funciona!";
+function leerFloat(bytes, offset) {
+    const view = new DataView(bytes.buffer);
+    return view.getFloat32(offset, true);
+}
+
+button.onclick = async () => {
+
+    const file = input.files[0];
+
+    if (!file) {
+        info.textContent = "No seleccionaste ningún archivo.";
+        return;
+    }
+
+    const buffer = await file.arrayBuffer();
+    const bytes = new Uint8Array(buffer);
+
+    let texto = "";
+
+    for (let i = 28; i < 100; i += 4) {
+        texto += "Offset " + i + " = " + leerFloat(bytes, i) + "\n";
+    }
+
+    info.textContent = texto;
 };
